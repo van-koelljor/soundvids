fe501e0669108868986c93002a76de5f5218d386
Joris Köllhofer <koellhofer@tutanota.com>
2024-05-15 18:45:37 +0200

    feat(settings): add store

---
+++ src/stores/settings.ts
@@ -0,0 +1,19 @@
+import { defineStore } from 'pinia'
+
+export const useSettingsStore = defineStore('settings', {
+  state: () => ({
+    videoOutputPath: ''
+  }),
+  actions: {
+    setVideoOutputPath(path: string) {
+      this.videoOutputPath = path;
+      window.electronAPI.setVideoOutputPath(path);
+    },
+    async loadVideoOutputPath() {
+      const result = await window.electronAPI.getVideoOutputPath();
+      if (result.success) {
+        this.videoOutputPath = result.path;
+      }
+    }
+  }
+});
