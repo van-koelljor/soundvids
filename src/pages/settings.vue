76eea6cc16ce73a8bbe64ffe51985ec17353fc6b
Joris Köllhofer <koellhofer@tutanota.com>
2024-05-15 18:52:52 +0200

    feat(settings): add page

---
+++ src/pages/settings.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import SettingsVideo from '@/components/Settings/SettingsVideo.vue';
+definePage({
+  meta: {
+    title: 'Settings',
+    icon: 'mdi-cog',
+    drawerIndex: 4,
+    breadcrumb: 'disabled',
+  },
+})
+</script>
+
+<template>
+  <SettingsVideo />
+</template>
