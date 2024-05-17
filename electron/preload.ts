8cb321dfc24984564a12f16bc2366f39f1d36f9e
Joris Köllhofer <koellhofer@tutanota.com>
2024-05-17 15:18:06 +0200

    feat(electron): implement IPC communication

---
+++ electron/preload.ts
@@ -0,0 +1,15 @@
+import { contextBridge, ipcRenderer } from 'electron';
+
+contextBridge.exposeInMainWorld('electronAPI', {
+  getTempDir: () => ipcRenderer.invoke('getTempDir'),
+  toggleDark: (dark: boolean) => ipcRenderer.invoke('darkMode:toggle', dark),
+  createVideo: (imagePath, audioPath, outputPath) => ipcRenderer.invoke('create-video', imagePath, audioPath, outputPath),
+  writeFile: async (path, data) => {
+    const { writeFile } = await import('fs/promises');
+    return writeFile(path, data);
+  },
+  deleteFile: (path) => ipcRenderer.invoke('delete-file', path),
+  joinPath: (...paths) => require('path').join(...paths),
+  openDirectoryDialog: () => ipcRenderer.invoke('open-directory-dialog'),
+  setVideoOutputPath: (path) => ipcRenderer.invoke('setVideoOutputPath', path)
+});
