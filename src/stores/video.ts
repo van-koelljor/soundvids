3eeb233c7342885ba5030bd6edf08f88a57c279c
Joris Köllhofer <koellhofer@tutanota.com>
2024-05-17 16:08:20 +0200

    refactor(video-store): enforce type safety with Video interface

--- src/stores/video.ts
+++ src/stores/video.ts
@@ -1,14 +1,15 @@
 import { defineStore } from 'pinia'
+import type { Video } from '@/types/video'
 
 export const useVideoStore = defineStore('video', {
   state: () => ({
-    createdVideos: []
+    createdVideos: [] as Video[]
   }),
   actions: {
-    addCreatedVideo(video) {
+    addCreatedVideo(video: Video) {
       this.createdVideos.push(video);
     },
-    removeCreatedVideo(id) {
+    removeCreatedVideo(id: string) {
       this.createdVideos = this.createdVideos.filter(video => video.id !== id);
     }
   }
