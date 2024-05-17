94d4aeee37dd926bbddbf329578e812b29f9b429
Joris Köllhofer <koellhofer@tutanota.com>
2024-05-17 15:20:06 +0200

    feat(video-creator): add page

---
+++ src/pages/videoCreator.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import VideoCreator from '@/components/VideoCreator/VideoCreator.vue'
+definePage({
+  meta: {
+    icon: 'mdi-video',
+    title: 'Video Creator',
+    drawerIndex: 1,
+  },
+})
+</script>
+
+<template>
+  <v-container fluid>
+    <VideoCreator />
+  </v-container>
+</template>
